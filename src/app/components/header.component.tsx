deac39a8b37f3886ce28e6bfa42d0dfe393b1922
vientorepublic <markgod0419@gmail.com>
2024-09-10 08:36:29 +0900

    refactoring: crypto module

--- src/app/components/header.component.tsx
+++ src/app/components/header.component.tsx
@@ -7,7 +7,7 @@ export default function HeaderComponent() {
     <nav className="bg-gray-100 dark:bg-gray-800 fixed w-full z-20 top-0 start-0 dark:border-gray-600 shadow-md">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/" className="flex items-center rtl:space-x-reverse">
-          <Image src={Logo} width={35} height={35} alt="" />
+          <Image src={Logo} width={35} height={35} alt="" priority />
           <span className="text-2xl font-bold brand-gradient mt-1">Safe Share</span>
         </Link>
       </div>
